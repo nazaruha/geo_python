82b8636c0cc2cdd2acf042a69e90f8367a98b076
Nazar <fedunnazar20122001@gmail.com>
2023-04-21 14:52:36 +0300

    add 404 error functionality

--- Lab_08/app.py
+++ Lab_08/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify  # Flask - локальний сервер
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
 
 @app.route('/get_json/<string:filename>', methods=['GET'])
 def get_json(filename):
@@ -15,5 +17,16 @@ def get_json(filename):
     except FileNotFoundError:
         return jsonify({'error': 'File not found'})
 
+
+@app.errorhandler(404)  # page doesn't exists
+def page_not_found(error):
+    msg_not_found = "<div style=\"display: flex; justify-content: center\">\n\t<div style=\"display: flex; flex-direction: column\">\n\t\t<h1>Page isn't found</h1>"
+    msg_go_home = "\n\t\t<a href=\"/\">Home Page</a>\n\t</div>\n</div>"
+    msg = msg_not_found + msg_go_home
+    print(msg)
+    return msg, 404
+
+
 if __name__ == '__main__':
+    # start server. 'debug=True' param lets to track errors in the app and show them on the web-page in the browser during developer work
     app.run(debug=True)
